3fbae8e3858be61171f5a26358bef29c8b40be79
Rozanov Ivan <navi.zor9@gmail.com>
2021-12-09 22:14:54 +0300

    add constructors

--- Курсовая работа (консоль)/Array.h
+++ Курсовая работа (консоль)/Array.h
@@ -3,10 +3,37 @@
 template <class T>
 class Array
 {
-public:
+private:
 	T* data;
 	size_t size;
 	size_t capacity;
-private:
 
+	T* allocate();
+public:
+	Array();
+	Array(const Array&);
 };
+
+template<class T>
+inline T* Array<T>::allocate()
+{
+	return new T[capacity];
+}
+
+template<class T>
+inline Array<T>::Array()
+	: size(0), capacity(0), data(nullptr)
+{
+}
+
+template<class T>
+inline Array<T>::Array(const Array& src)
+{
+	this->size = src.size;
+	this->capacity = src.size;
+	this->allocate();
+	for (size_t i = 0; i < size; i++)
+	{
+		this->data[i] = src.data();
+	}
+}

cc7b3f85054ed03b9d4cd6748f6dadda07b684d0
Rozanov Ivan <navi.zor9@gmail.com>
2021-12-10 03:23:18 +0300

    fix copy constructors change size of buffer (m_size -> m_capacity)

--- Курсовая работа (консоль)/String.cpp
+++ Курсовая работа (консоль)/String.cpp
@@ -16,7 +16,7 @@ String::String(const String& src)
 	: m_size(src.m_size), m_capacity(src.m_size + 1)
 {
 	this->str = this->allocate();
-	strcpy(this->str, src.str);
+	strcpy_s(this->str, m_capacity * sizeof(char), src.str);
 }
 
 String::String(const char* src)
@@ -24,5 +24,5 @@ String::String(const char* src)
 	m_size = strlen(src);
 	m_capacity = m_size + 1;
 	this->str = allocate();
-	strcpy(this->str, src);
+	strcpy_s(this->str, m_capacity * sizeof(char), src);
 }

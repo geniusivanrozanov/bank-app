b9acae9372dceab6400220b734ae67f4ee2fea52
Rozanov Ivan <navi.zor9@gmail.com>
2021-12-10 03:05:12 +0300

    add constructors

--- Курсовая работа (консоль)/String.cpp
+++ Курсовая работа (консоль)/String.cpp
@@ -1,1 +1,28 @@
 #include "String.h"
+
+char* String::allocate()
+{
+	return new char[m_capacity];
+}
+
+String::String()
+	: m_size(0), m_capacity(15)
+{
+	str = allocate();
+	str[m_size] = '\0';
+}
+
+String::String(const String& src)
+	: m_size(src.m_size), m_capacity(src.m_size + 1)
+{
+	this->str = this->allocate();
+	strcpy(this->str, src.str);
+}
+
+String::String(const char* src)
+{
+	m_size = strlen(src);
+	m_capacity = m_size + 1;
+	this->str = allocate();
+	strcpy(this->str, src);
+}
--- Курсовая работа (консоль)/String.h
+++ Курсовая работа (консоль)/String.h
@@ -1,9 +1,18 @@
 #pragma once
+#include <cstring>
+
 class String
 {
 private:
+	char* str;
+	size_t m_size;
+	size_t m_capacity;
+
+	char* allocate();
 
 public:
-
+	String();
+	String(const String&);
+	String(const char*);
 };
 

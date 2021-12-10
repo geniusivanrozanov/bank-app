b5aae3ece8ff44e23e8f92542475f12fd2df1b38
Rozanov Ivan <navi.zor9@gmail.com>
2021-12-10 19:49:14 +0300

    add destroyToDown() and destroyToUp()

--- Курсовая работа (консоль)/Stack.h
+++ Курсовая работа (консоль)/Stack.h
@@ -14,24 +14,24 @@ public:
 
 /*
 				<-Moving DOWN->
-	+---+			+---+			+---+			* <- head
-	| * |			|   |			|   |
+	+---+			+---+			+---+		* <- head
+	| * |	 UP		|   |	 UP		|   |
 	+---+			+---+			+---+
 	|   |			| * |			|   |
 	+---+			+---+			+---+
 	|   |			|   |			| * |
 	+---+			+---+			+---+
-	|   |			|   |			|   |
+	|   |	DOWN	|   |	DOWN	|   |
 	+---+			+---+			+---+
 				<-Moving UP->
 	+---+			+---+			+---+
-	|   |			|   |			| * |
+	|   |	 UP		|   |	 UP		| * |
 	+---+			+---+			+---+
 	|   |			| * |			|   |
 	+---+			+---+			+---+
 	| * |			|   |			|   |
 	+---+			+---+			+---+
-	|   |			|   |			|   |
+	|   |	DOWN	|   |	DOWN	|   |
 	+---+			+---+			+---+
 */
 
@@ -41,4 +41,43 @@ class Stack
 private:
 	Node<T>* head;
 
+	void destroyToDown();
+	void destroyToUp();
+public:
+	Stack();
+
 };
+
+template<class T>
+inline void Stack<T>::destroyToDown()
+{
+	Node<T>* prev = head->prev;
+	Node<T>* temp;
+	head->prev = nullptr;
+	while (prev != nullptr)
+	{
+		temp = prev->prev;
+		delete prev;
+		prev = temp;
+	}
+}
+
+template<class T>
+inline void Stack<T>::destroyToUp()
+{
+	Node<T>* next = head->next;
+	Node<T>* temp;
+	head->next = nullptr;
+	while (next != nullptr)
+	{
+		temp = next->next;
+		delete next;
+		next = temp;
+	}
+}
+
+template<class T>
+inline Stack<T>::Stack()
+	: head(nullptr)
+{
+}
